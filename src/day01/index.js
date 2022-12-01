4137349c7b6b6164a71f0d1cb8a7537f8ee83965
Gerard Degas <gdegas@squareup.com>
2022-12-01 15:58:59 -0800

    day 1 complete

--- src/day01/index.js
+++ src/day01/index.js
@@ -2,41 +2,89 @@ import run from "aocrunner"
 
 const parseInput = (rawInput) => rawInput
 
-const main = (firstNum, secondNum) => {
-  return firstNum + secondNum
+const sumCalories = (input) => {
+  const split = input.split("\n")
+
+  const result = []
+  let sum = 0
+  for (const amt of split) {
+    if (amt) {
+      sum += Number(amt)
+    } else {
+      result.push(sum)
+      sum = 0
+    }
+  }
+  if (sum) {
+    result.push(sum)
+  }
+  return result
 }
+
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
-  console.log("input", input)
-
-  return main(2, 3)
+  const calories = sumCalories(input)
+  calories.sort((a, b) => a - b)
+  return calories.at(-1)
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  const calories = sumCalories(input)
 
-  return
+  calories.sort((a, b) => a - b)
+  return calories.at(-1) + calories.at(-2) + calories.at(-3)
 }
 
 run({
   part1: {
     tests: [
       {
-        input: `1`,
-        expected: 4,
+        input: `
+              1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+              `,
+        expected: 24000,
       },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+              1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+              `,
+        expected: 45000,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 })
